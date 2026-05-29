0f154dfad6d1d95422dcfa8df7e53aafd8a1f147
Jmr13 <Gamayanjomar@yahoo.com>
2026-05-29 16:39:19 +0800

    chore: Updated requirements.txt to remove some dependencies

--- src/tui/ui_component_factory.py
+++ src/tui/ui_component_factory.py
@@ -1,12 +1,7 @@
 from abc import ABC, abstractmethod
 from src.tui.ui_components import UIComponent, TitleBar, EntryList, EntryDetails
-
-class UIComponentFactoryInterface(ABC):
-    @abstractmethod
-    def create_component(self, component_type, **kwargs):
-        pass
     
-class UIComponentFactory(UIComponentFactoryInterface):
+class UIComponentFactory():
     @staticmethod
     def create_component(component_type, **kwargs) -> UIComponent:
         if component_type == "title_bar":
